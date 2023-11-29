84fbce92fff845993d8d2e8abe7f222cb27b11b0
Lorena Fuente <lfuente@birt.eus>
2023-11-29 19:15:02 +0100

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'di02_te01',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
